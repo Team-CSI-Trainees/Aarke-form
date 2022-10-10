48c908a977f014fb592e8b7facf6b2713b7543c4
AarkeTripathi <113425044+AarkeTripathi@users.noreply.github.com>
2022-10-10 22:57:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function valiadteform() 
+{
+    var name= document.getElementById("name")
+    var contact= document.getElementById("contact")
+    var email= document.getElementById("email")
+    var password= document.getElementById("password")
+    var confirm= document.getElementById("confirm")
+    if(name)
+}
